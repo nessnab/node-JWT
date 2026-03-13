365059dbcdaf1ece9d2f87f8e45d0c21ce3d4345
nessnab <112866526+nessnab@users.noreply.github.com>
2026-03-13 20:58:23 +0700

    lesson-7: hashing password using bcrypt

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -18,13 +19,15 @@ const userSchema = new mongoose.Schema({
 
 // fire a function before doc saved to db
 userSchema.pre('save', async function () {
-  console.log('user about to be created & saved', this);
+  // console.log('user about to be created & saved', this);
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // fire a function after doc saved to db
-userSchema.post('save', async function (doc) {
-  console.log('new user was created & saved', doc);
-});
+// userSchema.post('save', async function (doc) {
+//   console.log('new user was created & saved', doc);
+// });
 
 
 const User = mongoose.model('user', userSchema);
