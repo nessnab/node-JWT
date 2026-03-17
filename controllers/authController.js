07c77ce6dff7c53906261f356ea2e96c7cd0f780
nessnab <112866526+nessnab@users.noreply.github.com>
2026-03-17 19:21:24 +0700

    lesson-10: user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/users');
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
@@ -20,6 +21,14 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+  return jwt.sign({ id, }, 'martiny is cute secret', {
+    expiresIn: maxAge
+  })
+}
+
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 }
@@ -31,7 +40,9 @@ module.exports.signup_post = async (req, res) => {
   
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+    res.status(201).json({user: user._id});
   }
   catch (err) {
     const errors = handleErrors(err);
